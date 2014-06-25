ead9ae94f16d86b1931e3098b66da35a552ede55
aramik <aramik@whalerockindustries.com>
2014-06-25 13:49:14 -0700

    testing db

--- includes/_dbconfig.php
+++ includes/_dbconfig.php
@@ -2,10 +2,9 @@
 echo "welcome";
 $conn = mysql_connect('localhost', 'root', 'Cmb1h0aJ8v');
 if (!$conn) {
-	mysql_select_db("admin", $conn)
     die('Could not connect: ' . mysql_error());
 }
-
+mysql_select_db("admin", $conn);
 $sql = "SELECT * FROM posts";
 $rs = mysql_query($sql, $conn) or die ('Problem with query' . mysql_error());
 echo 'Connected successfully';
