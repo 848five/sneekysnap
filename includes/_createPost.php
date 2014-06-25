f1185d97687464814b3ea96c10249958673354ef
aramik <aramik@whalerockindustries.com>
2014-06-25 14:28:25 -0700

    testing db

---
+++ includes/_createPost.php
@@ -0,0 +1,30 @@
+<?php 
+include '_dbcongif.php';
+
+mysql_select_db("admin", $conn);
+$sql = "SELECT * FROM posts";
+
+$title = $_REQUEST['title'];
+$deck = $_REQUEST['deck'];
+$article = $_REQUEST['article'];
+$images = $_REQUEST['images'];
+
+
+$sql="INSERT INTO posts (title, deck, article, images)
+VALUES ('$title','$deck','$article','$images')";
+
+if (!mysqli_query($conn,$sql)) {
+  die('Error: ' . mysqli_error($con));
+}
+echo "1 record added";
+
+
+// $rs = mysql_query($sql, $conn) or die ('Problem with query' . mysql_error());
+// echo 'Connected successfully';
+
+// while ($row = mysql_fetch_array($rs)) {
+// 	echo $row['id'];
+// }
+
+mysql_close($conn);
+?>
