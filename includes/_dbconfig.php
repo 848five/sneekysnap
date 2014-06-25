5ee84edfc8274cf98acfc5e887cef2a56ebad808
aramik <aramik@whalerockindustries.com>
2014-06-25 14:27:59 -0700

    testing db

--- includes/_dbconfig.php
+++ includes/_dbconfig.php
@@ -4,14 +4,5 @@ $conn = mysql_connect('localhost', 'root', 'Cmb1h0aJ8v');
 if (!$conn) {
     die('Could not connect: ' . mysql_error());
 }
-mysql_select_db("admin", $conn);
-$sql = "SELECT * FROM posts";
-$rs = mysql_query($sql, $conn) or die ('Problem with query' . mysql_error());
-echo 'Connected successfully';
 
-while ($row = mysql_fetch_array($rs)) {
-	echo $row['id'];
-}
-
-mysql_close($conn);
 ?>
