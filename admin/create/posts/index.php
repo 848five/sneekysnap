2cb351cc7a8cc2dee1bb98a1db6c634ee723b502
aramik <aramik@whalerockindustries.com>
2014-06-26 17:54:22 -0700

    added create form files

---
+++ admin/create/posts/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+?>
+
+
+<section id="create-post">
+	<form method="post" action="<?$_SERVER['DOCUMENT_ROOT']."/includes/_createPosts.php"?>">
+		<input type="text" name="title" />
+		<input type="text" name="deck" />
+		<textarea type="text" name="article"></textarea>
+		<input type="text" name="images" />
+		<input class="button" type="submit"></input>
+	</form>
+</section>
