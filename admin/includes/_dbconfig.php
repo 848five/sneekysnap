545a393bc0d89e9e82ff30f9f8e5f16db017b5b0
aramik <aramik@whalerockindustries.com>
2014-06-30 14:38:16 -0700

    updated read posts

--- admin/includes/_dbconfig.php
+++ admin/includes/_dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect("localhost","root","Cmb1h0aJ8v");
+$con=mysql_connect("localhost","root","Cmb1h0aJ8v","admin");
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
