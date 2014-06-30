b46f0831d68b9f70296454fc79535ab8a0757659
aramik <aramik@whalerockindustries.com>
2014-06-30 13:54:59 -0700

    fixed syntax error

--- admin/includes/read/_posts.php
+++ admin/includes/read/_posts.php
@@ -6,14 +6,14 @@ include($_SERVER['DOCUMENT_ROOT']."/admin/includes/_dbconfig.php");
 $SQL = "SELECT * FROM posts";
 $result = mysql_query($SQL);
 
-echo "-----posts-><br />"
+echo "-----posts-><br />";
 while ( $db_field = mysql_fetch_assoc($result) ) {
 
 print $db_field['title'] . "<BR>";
 
 
 }
-echo "end posts-----"
+echo "end posts-----";
 
 // $rs = mysql_query($sql, $con) or die ('Problem with query' . mysql_error());
 // echo 'conected successfully';
