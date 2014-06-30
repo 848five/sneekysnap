20a67f40d4073bdef2acf390ca5e0f3a2cbba423
aramik <aramik@whalerockindustries.com>
2014-06-30 13:40:53 -0700

    u2

--- admin/create/post/index.php
+++ admin/create/post/index.php
@@ -4,7 +4,7 @@
 
 
 <section id="create-post">
-	<form method="post" action="../..//includes/_createPost.php">
+	<form method="post" action="../includes/_createPost.php">
 		<input type="text" name="title" />
 		<input type="text" name="deck" />
 		<textarea type="text" name="article"></textarea>
