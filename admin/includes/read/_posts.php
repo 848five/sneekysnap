e17c91a22b3528691711b336ac58721e7fa05152
aramik <aramik@whalerockindustries.com>
2014-06-30 14:00:29 -0700

    updated read posts

--- admin/includes/read/_posts.php
+++ admin/includes/read/_posts.php
@@ -4,7 +4,7 @@ include($_SERVER['DOCUMENT_ROOT']."/admin/includes/_dbconfig.php");
 
 
 mysql_select_db('admin');
-$retval = mysql_query( $sql, $conn );
+$retval = mysql_query( $sql, $con );
 if(! $retval )
 {
   die('Could not get data: ' . mysql_error());
